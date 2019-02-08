f8af54b46d225b21fc949d9db374e8b5716bc28c
AnDrOlEiN <syrogk@gmail.com>
2019-02-08 18:52:14 +0300

    added linting

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
  * @param {string} locale
  * @return {string}
  */
-module.exports = function(number, locale) {
+module.exports = function (number, locale) {
   return number.toLocaleString(locale);
 };
